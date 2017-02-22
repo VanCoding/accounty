e75c2f46121d7383a9fe8bee7d3b269e7d45b18c
Patrik Stutz <patrik.stutz@gmail.com>
2017-02-22 18:20:36 +0100

    automatically create income statement account

--- src/api/periods/create.js
+++ src/api/periods/create.js
@@ -6,6 +6,8 @@ module.exports = async function(c){
 	var body = await parse.json(c);
 	var query = p.insert(body).returning(p.id);
 	var period = await c.db.one(query.toQuery());
+	var query = a.insert({period:period.id+"",name:"Erfolgsrechnung",number:"2",type:3,active:true})
+	await c.db.none(query.toQuery());
 	c.set("Content-Type","text/plain");
 	c.body = period.id+"";
 }
